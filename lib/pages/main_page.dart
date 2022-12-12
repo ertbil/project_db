cba29e9ab9395c90d78159dcaa8096fd01edc555
Ertuğrul Bilgiç <bilgicertugrul6@gmail.com>
2022-12-12 11:21:24 +0300

    CarouselSlider added

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -1,11 +1,152 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:project_db/components/custom_text_form_field.dart';
+
+
+final List<String> imgList = [
+  'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+  'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  'https://images.unsplash.com/photo-1523205771623-e0faa4d2813d?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=89719a0d55dd05e2deae4120227e6efc&auto=format&fit=crop&w=1953&q=80',
+  'https://images.unsplash.com/photo-1508704019882-f9cf40e475b4?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=8c6e5e3aba713b17aa1fe71ab4f0ae5b&auto=format&fit=crop&w=1352&q=80',
+  'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80'
+];
+
+final themeMode = ValueNotifier(2);
+
+class CarouselDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder(
+      builder: (context, value, g) {
+        return MaterialApp(
+          initialRoute: '/',
+          darkTheme: ThemeData.dark(),
+          themeMode: ThemeMode.values.toList()[value as int],
+          debugShowCheckedModeBanner: false,
+          routes: {
+
+            '/indicator': (ctx) => CarouselWithIndicatorDemo(),
+
+          },
+        );
+      },
+      valueListenable: themeMode,
+    );
+  }
+}
+
+final List<Widget> imageSliders = imgList
+    .map((item) => Container(
+  child: Container(
+    margin: EdgeInsets.all(5.0),
+    child: ClipRRect(
+        borderRadius: BorderRadius.all(Radius.circular(5.0)),
+        child: Stack(
+          children: <Widget>[
+            Image.network(item, fit: BoxFit.cover, width: 1000.0),
+            Positioned(
+              bottom: 0.0,
+              left: 0.0,
+              right: 0.0,
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color.fromARGB(200, 0, 0, 0),
+                      Color.fromARGB(0, 0, 0, 0)
+                    ],
+                    begin: Alignment.bottomCenter,
+                    end: Alignment.topCenter,
+                  ),
+                ),
+                padding: EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 20.0),
+                child: Text(
+                  'No. ${imgList.indexOf(item)} image',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        )),
+  ),
+))
+    .toList();
+
+class CarouselWithIndicatorDemo extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _CarouselWithIndicatorState();
+  }
+}
+
+class _CarouselWithIndicatorState extends State<CarouselWithIndicatorDemo> {
+  int _current = 0;
+  final CarouselController _controller = CarouselController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: Column(children: [
+        Expanded(
+          child: CarouselSlider(
+            items: imageSliders,
+            carouselController: _controller,
+            options: CarouselOptions(
+                autoPlay: true,
+                enlargeCenterPage: true,
+                aspectRatio: 2.0,
+                onPageChanged: (index, reason) {
+                  setState(() {
+                    _current = index;
+                  });
+                }),
+          ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: imgList.asMap().entries.map((entry) {
+            return GestureDetector(
+              onTap: () => _controller.animateToPage(entry.key),
+              child: Container(
+                width: 12.0,
+                height: 12.0,
+                margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: (Theme.of(context).brightness == Brightness.dark
+                        ? Colors.white
+                        : Colors.black)
+                        .withOpacity(_current == entry.key ? 0.9 : 0.4)),
+              ),
+            );
+          }).toList(),
+        ),
+      ]),
+    );
+  }
+}
+
 
 class MainPage extends StatelessWidget {
   const MainPage({Key? key}) : super(key: key);
 
+
+
   @override
   Widget build(BuildContext context) {
+    SliverGridDelegate gridDelegate = SliverGridDelegateWithFixedCrossAxisCount(
+      crossAxisCount: 2,
+      mainAxisSpacing: 10,
+      crossAxisSpacing: 10,
+      childAspectRatio: 1,
+    );
     return Scaffold(
       appBar: AppBar(
         title: const Text("FriendYol"),
@@ -26,9 +167,71 @@ class MainPage extends StatelessWidget {
             }, icon: const Icon(Icons.shopping_cart)),
           ),
 
+          Padding(padding: const EdgeInsets.all(8.0),
+            child: IconButton(onPressed: () {
+
+            }, icon: CircleAvatar(radius: 20,child: const Icon(Icons.person, size: 20,),),),
+          ),
+
 
         ],
       ),
+
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              height: MediaQuery.of(context).size.height * 0.5,
+              child: CarouselWithIndicatorDemo(),
+            ),
+          ),
+
+          SliverGrid(
+            delegate: SliverChildBuilderDelegate(
+                  (context, index) {
+                return Card(
+                  elevation: 5,
+                  child: Column(
+                    children: [
+                      Expanded(
+                        child: Image.network(
+                          'https://images.unsplash.com/photo-1519985176271-adb1088fa94c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=a0c8d632e977f94e5d312d9893258f59&auto=format&fit=crop&w=1355&q=80',
+                          fit: BoxFit.cover,
+                        ),
+
+                      ),
+
+                      Text('Product Name'),
+                      Text('Price'),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.end,
+                        children: [
+                          IconButton(
+                            icon: Icon(  Icons.favorite),
+
+                            onPressed: () {
+
+                            },
+                          ),
+                          IconButton(
+                              icon: const Icon(Icons.arrow_forward_outlined)
+                              , onPressed: () {
+
+                          })
+                        ],
+                      )
+                    ],
+                  ),
+                );
+              },
+              childCount: 10,
+            ),
+            gridDelegate: gridDelegate,
+          ),
+        ],
+      ),
+
+
     );
   }
 }
